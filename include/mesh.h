c85d98db86beefee1e453b2027cb3dc9b3eaac69
antash-mishra <mishraantash34@gmail.com>
2025-05-05 08:00:51 +0530

    add class for mesh creation and imported assimp for model loading

---
+++ include/mesh.h
@@ -0,0 +1,36 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "shader.h"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+struct Vertex {
+  glm::vec3 Position;
+  glm::vec3 Normal;
+  glm::vec3 TexCoords;
+};
+
+struct Texture {
+  unsigned int id;
+  std::string type;
+};
+
+class Mesh {
+  public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(Shader &shader);
+
+  private:
+    // render data
+    unsigned int VAO, VBO, EBO;
+    void setupMesh();
+};
+
+#endif 
